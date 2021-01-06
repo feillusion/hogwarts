35b32991ffa83c06b38b6891ba08658e0f0ab0d1
feillusion <m15624008720@gmail.com>
2021-01-06 15:29:45 +0800

    pytest/allure复习

--- src/allure_test/__init__.py
+++ src/allure_test/__init__.py
@@ -1,3 +1,40 @@
+# 安装allure
+# 包下载：https://repo1.maven.org/maven2/io/qameta/allure/allure-commandline/2.13.7/
+# 解压->进入bin目录->运行allure.bat
+# 把bin目录添加到PATH环境变量
+# python安装allure-pytest包
+
+
 # 使用allure
-# pytest --alluredir=dirpath
-# allure serve dirpath
+# pytest filename.py  --alluredir=dirpath  执行结果存放在dirpath下
+# allure serve dirpath   启动服务并对dirpath下的数据生成报告，会直接打开默认浏览器查看，不会保存报告
+# pytest filename.py -s -q --alluredir=dirpath   在测试执行期间收集结果
+# allure generate dirpath -o reportpath --clean 把dirpath下的数据生成报告，存放在reportpath下，--clean用于覆盖
+# allure open -h 127.0.0.1 -p 8883 reportpath   打开reportpath下的报告查看
+
+
+# allure常用特性
+# 功能上加 @allure.feature('功能名称')
+# 子功能上加 @allure.story('子功能名称')
+# 步骤上加 @allure.step('步骤细节')
+# with allure.step('步骤细节'):  可以用在测试用例方法里，测试步骤放在with里面
+# @allure.attach('具体文本信息')，需要附加的信息，可以是数据，文本，图片，视频，网页
+# pytest filename.py --allure_features '功能名称' --allure_stories '子功能名称'  执行特定功能的用例
+
+# allure用例加链接
+# 关联bug：--allure-link-pattern=issue:url/{}
+# @allure.issue('{id}','textname')
+# @allure.testcase(url,'casetitle')
+# @allure.link(url,name='XXXX')
+
+# allure用例加重要级别
+# @allure.severity(allure.severity_level.TRIVIAL)
+# Blocker/critical/Normal/Minor/Trivial
+# 执行  pytest -sv 文件名 --allure-severities normal,critical
+
+# allure在测试报告里附加网页
+# @allure.attach(body,name,attachment_type,extension)
+# eg: @allure.attach('<head></head><body>首页</body>',‘这是错误页’,allure.attchment_type.HTML)
+# allure在测试报告里附加网页
+# @allure.attach.file(source,name,attachment_type,extension)
+# eg: @allure.attach.file('./result/a.png',‘这是错误页’,attachment_type=allure.attachment_type.PNG)
--- src/pytest_test/__init__.py
+++ src/pytest_test/__init__.py
@@ -8,10 +8,20 @@
 #pytest也可以执行unittest框架写的用例和方法
 
 #执行
-#pytest
+#pytest 文件名.py
+#pytest 文件名.py::类名
+#pytest 文件名.py::类名::方法名
 #-k 指定测试用例
-#-v 匹配包含名字的用例
+#-v 打印详细运行日志信息
 #-s 显示打印内容
+#-x 一旦运行报错，就停止运行
+#--maxfail=[num] 运行错误达到num时停止运行
+#-m [标记名] 运行带有[标记名]的用例
+#pytest --collect-only 只收集用例
+#pytest --junitxml=./result.xml 生成执行结果文件
+#pytest --setup-show 回溯fixture的执行过程
+
+
 
 #setup、teardown  类方法开始、结束时执行
 #setup_class、teardown_class  类开始、结束时执行
@@ -42,3 +52,15 @@
 # @pytest.mark.parametrize("a,b",yaml.safe_load(open(file_name)))
 # def test_param(self,a,b):
 #     print(a+b)
+
+
+#pytest 配置文件
+#同目录下创建  pytest.ini文件
+#[pytest]
+#markers=a b   自定义mark标签名
+#addopts=a b   运行时参数（可添加多个，用空格隔开，所有参数与命令行一致）
+#python_files 自定义测试文件命名规则
+#python_class-Test_* 自定义测试类命名规则
+#python_functions-test_* check_* 自定义测试方法命名规则
+#testpaths - path1 path2
+#norecursedir - dir1 dir2 运行时忽略某些文件夹
