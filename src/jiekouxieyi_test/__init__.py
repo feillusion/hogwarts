befa524072f71cf4f380f8f4b4960667d8d3b6b3
feillusion <m15624008720@gmail.com>
2021-02-27 19:02:16 +0800

    update

--- src/jiekouxieyi_test/__init__.py
+++ src/jiekouxieyi_test/__init__.py
@@ -1,4 +1,8 @@
 '''
     参考学习视频第9阶段
     参考文档：https://www.cnblogs.com/zhaikunkun/category/1763344.html
+
+    mitmproxy安装
+    pip install mitmproxy 如果失败了，先试试升级pip到最新版本，还不行的话再试试网上的其他方法
+                (试了很多其他方法都不行，最后升级了pip之后直接安装就成功了orz)
 '''
